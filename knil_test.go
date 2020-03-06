acb4daddb8b23c0a16e24d967eaf29fb517c85bf
Matts966 <Matts966@users.noreply.github.com>
2020-03-06 16:32:08 +0900

    Validate analyzer in test

--- knil_test.go
+++ knil_test.go
@@ -7,11 +7,13 @@ package knil_test
 import (
 	"testing"
 
+	"golang.org/x/tools/go/analysis"
 	"golang.org/x/tools/go/analysis/analysistest"
 	"github.com/Matts966/knil"
 )
 
 func Test(t *testing.T) {
+	analysis.Validate([]*analysis.Analyzer{knil.Analyzer})
 	testdata := analysistest.TestData()
 	analysistest.Run(t, testdata, knil.Analyzer, "nil")
 }
