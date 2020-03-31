c0487b9342c37972ab050254dd432bf24740bba5
Matts966 <Matts966@users.noreply.github.com>
2020-03-31 15:55:00 +0900

    Use gitsubmodule as first step toward implementing fullchecker

--- cmd/knil/main.go
+++ cmd/knil/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/Matts966/knil/analyzer/knil"
-	"golang.org/x/tools/go/analysis/singlechecker"
+	"github.com/Matts966/knil/fullchecker"
 )
 
-func main() { singlechecker.Main(knil.Analyzer) }
+func main() { fullchecker.Main(knil.Analyzer) }
